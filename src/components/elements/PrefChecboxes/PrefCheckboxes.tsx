41435294ab9d1405fdcc3e206f0163a3708849b5
nyk9 <eijikagimoto@gmail.com>
2024-11-18 14:39:02 +0900

    feat: チェックしたものを状態管理する
    usePrefecturesフックで管理するので、インポートし、チェックボックスのonChangeに関数を記述した

--- src/components/elements/PrefChecboxes/PrefCheckboxes.tsx
+++ src/components/elements/PrefChecboxes/PrefCheckboxes.tsx
@@ -1,14 +1,20 @@
-"use clinet";
+"use client";
 
+import { usePrefectures } from "@/hooks/usePrefectures";
 import { PrefecturesResponse } from "@/types/prefectures/prefectures";
 
 export function PrefCheckboxes(data: { prefs: PrefecturesResponse | undefined }) {
+  const { handleCheckedPrefectures } = usePrefectures();
   return (
     <div>
       <form className="flex flex-wrap ">
         {data.prefs?.result.map((pref) => {
           return (
-            <label key={pref.prefCode} className="p-1 flex-row w-1/6">
+            <label
+              key={pref.prefCode}
+              className="p-1 flex-row w-1/6"
+              onChange={() => handleCheckedPrefectures(pref.prefCode)}
+            >
               <input type="checkbox" value={pref.prefCode} />
               <span>{pref.prefName}</span>
             </label>
