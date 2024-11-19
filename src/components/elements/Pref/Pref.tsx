8c2f4d3e43e0e5ba8b6163262456853beaaa95b4
nyk9 <eijikagimoto@gmail.com>
2024-11-19 10:51:25 +0900

    feat: クライアントコンポーネントを作成・表示した
    クライアントコンポーネントでは、子コンポーネントにチェックボックスとグラフコンポーネントを持つ

---
+++ src/components/elements/Pref/Pref.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { usePrefectures } from "@/hooks/usePrefectures";
+import { PrefCheckboxes } from "../PrefChecboxes/PrefCheckboxes";
+import { PopulationResponse } from "@/types/populations/populations";
+import { PrefecturesResponse } from "@/types/prefectures/prefectures";
+import PopulationLine from "../PopulationLine/PopulationLine";
+
+export default function Pref(params: {
+  prefs: PrefecturesResponse;
+  populations: PopulationResponse[];
+}) {
+  const { checkedPrefectures, handleCheckedPrefectures } = usePrefectures();
+  return (
+    <>
+      <PrefCheckboxes
+        prefs={params.prefs}
+        handleFunc={(prefCode: number) => handleCheckedPrefectures(prefCode)}
+      />
+      <PopulationLine
+        populations={params.populations}
+        prefectures={params.prefs}
+        checkedPrefectures={checkedPrefectures}
+      />
+    </>
+  );
+}
