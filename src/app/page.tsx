e8f3aefcbbb2a4d89dcf714af1cb9e9cba698bfc
nyk9 <eijikagimoto@gmail.com>
2024-12-02 09:03:10 +0900

    refactor: ページコンポーネントの不要なコードを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import PageViewer from "@/components/elements/PageViewer/PageViewer";
 import { getPopulation } from "@/lib/getPopulation";
 import { getPrefectures } from "@/lib/getPrefectures";
 import { PopulationResponse } from "@/types/populations/populations";
-import { Suspense } from "react";
 
 export const experimental_ppr = true;
 
@@ -21,11 +20,7 @@ export default async function Home() {
   }
   return (
     <div className="items-center justify-items-center max-h-screen p-3 sm:p-10 font-[family-name:var(--font-geist-sans)]">
-      <Suspense fallback={<>loading</>}>
-        {/* <PrefCheckboxes prefs={prefectures} populations={populations} /> */}
-        <PageViewer prefs={prefectures} populations={populations} />
-      </Suspense>
-      {/* <PopulationLine populations={populations} prefectures={prefectures} /> */}
+      <PageViewer prefs={prefectures} populations={populations} />
     </div>
   );
 }
